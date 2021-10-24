e75ad5dc80652bfe7fd72f34868a72de6fbd6f58
Mihăiță Andrei <mihaitaandrei86@gmail.com>
2021-10-24 09:41:14 +0300

    |ADD| calculator partial ui

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -2,14 +2,37 @@ import './style.scss'
 import WindowControls from '../windowControls'
 import NumberInputArea from '../numberInputArea'
 import ButtonGridArea from '../buttonGridArea'
+import CalculatorPhoto from '../../images/mac.png'
+import { Textfit } from 'react-textfit'
+// function Calculator() {
+//     return (
+//         <section className="calculator">
+//             <WindowControls className="calculator__windowControls" />
+//             <NumberInputArea />
+//             <ButtonGridArea />
+//         </section>
+//     )
+// }
 
 function Calculator() {
     return (
-        <section>
-            <WindowControls />
-            <NumberInputArea />
-            <ButtonGridArea />
-        </section>
+        <>
+            <img src={CalculatorPhoto} alt="poza" />
+            <section className="calculator">
+                <div className="calculator__titleBar">
+                    <div class="titleBar__buttons">
+                        <div class="close"></div>
+                        <div class="minimize"></div>
+                        <div class="maximize"></div>
+                    </div>
+                </div>
+                <div className="calculator__inputField">
+                    <span>
+                        <Textfit mode="single">03213123213213213</Textfit>
+                    </span>
+                </div>
+            </section>
+        </>
     )
 }
 
